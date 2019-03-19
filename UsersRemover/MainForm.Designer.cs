b1d051dfc9abc3a1a4f76add240df8a701e86aa2
Roberto Carcamo <rcarcamo@voicenetworks.us>
2019-03-19 14:04:56 -0400

    Loops deleting all file in the designated directory

---
+++ UsersRemover/MainForm.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace UsersRemover
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.btnPath = new System.Windows.Forms.Button();
+            this.btnStopService = new System.Windows.Forms.Button();
+            this.lblPath = new System.Windows.Forms.Label();
+            this.lblSelectedPath = new System.Windows.Forms.Label();
+            this.StopProgramTimer = new System.Windows.Forms.Timer(this.components);
+            this.startTimer = new System.Windows.Forms.Timer(this.components);
+            this.bntTimerStop = new System.Windows.Forms.Button();
+            this.TimerProgress = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // btnPath
+            // 
+            this.btnPath.Location = new System.Drawing.Point(12, 12);
+            this.btnPath.Name = "btnPath";
+            this.btnPath.Size = new System.Drawing.Size(161, 23);
+            this.btnPath.TabIndex = 0;
+            this.btnPath.Text = "S&elect folder path";
+            this.btnPath.UseVisualStyleBackColor = true;
+            this.btnPath.Click += new System.EventHandler(this.btnPath_Click);
+            // 
+            // btnStopService
+            // 
+            this.btnStopService.Location = new System.Drawing.Point(179, 12);
+            this.btnStopService.Name = "btnStopService";
+            this.btnStopService.Size = new System.Drawing.Size(83, 23);
+            this.btnStopService.TabIndex = 1;
+            this.btnStopService.Text = "&Go";
+            this.btnStopService.UseVisualStyleBackColor = true;
+            this.btnStopService.Click += new System.EventHandler(this.btnStopService_Click);
+            // 
+            // lblPath
+            // 
+            this.lblPath.AutoSize = true;
+            this.lblPath.Location = new System.Drawing.Point(12, 38);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(32, 13);
+            this.lblPath.TabIndex = 2;
+            this.lblPath.Text = "Path:";
+            // 
+            // lblSelectedPath
+            // 
+            this.lblSelectedPath.AutoSize = true;
+            this.lblSelectedPath.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblSelectedPath.Location = new System.Drawing.Point(50, 38);
+            this.lblSelectedPath.Name = "lblSelectedPath";
+            this.lblSelectedPath.Size = new System.Drawing.Size(0, 13);
+            this.lblSelectedPath.TabIndex = 3;
+            // 
+            // startTimer
+            // 
+            this.startTimer.Interval = 5000;
+            this.startTimer.Tick += new System.EventHandler(this.startTimer_Tick);
+            // 
+            // bntTimerStop
+            // 
+            this.bntTimerStop.Location = new System.Drawing.Point(268, 12);
+            this.bntTimerStop.Name = "bntTimerStop";
+            this.bntTimerStop.Size = new System.Drawing.Size(83, 23);
+            this.bntTimerStop.TabIndex = 4;
+            this.bntTimerStop.Text = "S&top ";
+            this.bntTimerStop.UseVisualStyleBackColor = true;
+            this.bntTimerStop.Click += new System.EventHandler(this.bntTimerStop_Click);
+            // 
+            // TimerProgress
+            // 
+            this.TimerProgress.Location = new System.Drawing.Point(15, 54);
+            this.TimerProgress.Name = "TimerProgress";
+            this.TimerProgress.Size = new System.Drawing.Size(335, 19);
+            this.TimerProgress.TabIndex = 5;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(362, 85);
+            this.Controls.Add(this.TimerProgress);
+            this.Controls.Add(this.bntTimerStop);
+            this.Controls.Add(this.lblSelectedPath);
+            this.Controls.Add(this.lblPath);
+            this.Controls.Add(this.btnStopService);
+            this.Controls.Add(this.btnPath);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Users Remover";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPath;
+        private System.Windows.Forms.Button btnStopService;
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.Label lblSelectedPath;
+        private System.Windows.Forms.Timer StopProgramTimer;
+        private System.Windows.Forms.Timer startTimer;
+        private System.Windows.Forms.Button bntTimerStop;
+        private System.Windows.Forms.ProgressBar TimerProgress;
+    }
+}
+
---
+++ UsersRemover/MainForm.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System.Diagnostics;
+namespace UsersRemover
+{
+ 
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        
+        private void btnPath_Click(object sender, EventArgs e)
+        {   
+            // Opens the folder browser.
+            FolderBrowserDialog location = new FolderBrowserDialog();
+            location.ShowNewFolderButton = true;
+            DialogResult result = location.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                lblSelectedPath.Text = location.SelectedPath;
+            }
+
+        }
+
+        private void btnStopService_Click(object sender, EventArgs e)
+        {
+            // Starts the timer 
+            startTimer.Start();
+        }
+
+        private void startTimer_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                TimerProgress.Minimum = 0;
+                TimerProgress.Maximum = 100;
+
+                // Gets the folder path from the label and deletes all files in it.
+                string path = lblSelectedPath.Text;
+                string[] files = Directory.GetFiles(path);
+                foreach (string file in files)
+                {
+                    File.Delete(file);
+                    TimerProgress.Value = 0;
+                    TimerProgress.Value = 100;
+                }
+            }   
+            catch
+            {
+                // Message on label if no path is found. 
+                lblSelectedPath.Text = "Select the folder location";
+            }           
+        }
+
+        private void bntTimerStop_Click(object sender, EventArgs e)
+        {
+            startTimer.Stop();
+            lblSelectedPath.Text = "";
+            TimerProgress.Value = 0;
+        }
+    }
+}
